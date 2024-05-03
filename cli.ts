34329c4c01c04bee9a139797ce10aef3eaeb3874
Julien <julien@devanco.com>
2024-05-03 19:22:38 +0200

    launchable_url separated

--- cli.ts
+++ cli.ts
@@ -100,19 +100,47 @@ const asset_upload_all = async (otago_api_key: string) => {
   return expo_asset_upload_all(otago_api_key);
 };
 
+const manifest_signature = ({ manifest }: { manifest: Manifest }) => {
+  return null;
+};
+
 const send_manifest = async ({
   manifest_android,
   manifest_ios,
   otago_deployment_id,
   otago_project_id,
   otago_api_key,
+  otago_deploy_base_url,
 }: {
   manifest_android: Manifest | undefined; // used for TS checking
   manifest_ios: Manifest | undefined;
   otago_deployment_id: string;
   otago_project_id: string;
   otago_api_key: string;
+  otago_deploy_base_url: string;
 }) => {
+  const manifest_android_final = manifest_android
+    ? {
+        ...manifest_android,
+        launchAsset: {
+          ...manifest_android.launchAsset,
+          url: `${otago_deploy_base_url}/manifest/launchable?manifest_id=${manifest_android.id}&platform=android`,
+        },
+      }
+    : undefined;
+
+  const manifest_ios_final = manifest_ios
+    ? {
+        ...manifest_ios,
+        launchAsset: {
+          ...manifest_ios.launchAsset,
+          url: `${otago_deploy_base_url}/manifest/launchable?manifest_id=${manifest_ios.id}&platform=ios`,
+        },
+      }
+    : undefined;
+
+  //TODO signature
+
   return fetch(`http://localhost:3000/api/projects/${otago_project_id}/deployments/${otago_deployment_id}/activate`, {
     method: "POST",
     headers: {
@@ -120,8 +148,20 @@ const send_manifest = async ({
       "api-key": otago_api_key,
     },
     body: JSON.stringify({
-      android: manifest_android ? { manifest: manifest_android, signature: null } : undefined,
-      ios: manifest_ios ? { manifest: manifest_ios, signature: null } : undefined,
+      android: manifest_android
+        ? {
+            launchable_url: manifest_android.launchAsset.url,
+            manifest: manifest_android_final!,
+            signature: manifest_signature({ manifest: manifest_android_final! }),
+          }
+        : undefined,
+      ios: manifest_ios
+        ? {
+            launchable_url: manifest_ios.launchAsset.url,
+            manifest: manifest_ios_final!,
+            signature: manifest_signature({ manifest: manifest_ios_final! }),
+          }
+        : undefined,
     }),
   });
 };
@@ -210,7 +250,10 @@ const expo_main = async () => {
       config: { type: "expo", value: expo_config },
     }),
   });
-  const { deployment_id } = (await res_deployment.json()) as { deployment_id: string };
+  const { deployment_id, deploy_base_url } = (await res_deployment.json()) as {
+    deployment_id: string;
+    deploy_base_url: string;
+  };
 
   // upload assets
   const asset_manifest = await asset_upload_all(api_key);
@@ -234,8 +277,6 @@ const expo_main = async () => {
       })
     : undefined;
 
-  //TODO signature
-
   // activate deployment
   const res = await send_manifest({
     manifest_android,
@@ -243,6 +284,7 @@ const expo_main = async () => {
     otago_deployment_id: deployment_id,
     otago_project_id: "msjs-test",
     otago_api_key: api_key,
+    otago_deploy_base_url: deploy_base_url,
   });
 
   if (!res.ok) {
