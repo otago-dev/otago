c024f0fb89aedfe20fecb332f173e2cd27bf81c7
Julien <julien@devanco.com>
2024-06-14 18:05:28 +0200

    fix: commit tag missing in gitlab CI

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -1,10 +1,14 @@
 import { exec_command } from "./cli";
 
 export const get_current_git_version = async (root_dir: string) => {
+  let commit_tag_gitcli = undefined;
   try {
-    const stdout = await exec_command(root_dir, "git log --pretty=tformat:%h -n1");
-    return stdout.trim();
-  } catch (error) {
-    return "";
-  }
+    const stdout = await exec_command(root_dir, "git rev-parse --short HEAD");
+    commit_tag_gitcli = stdout.trim();
+  } catch (error) {}
+
+  const commit_tag_github = (process.env.GITHUB_SHA || "").substring(0, 7);
+  const commit_tag_gitlab = process.env.CI_COMMIT_SHORT_SHA;
+
+  return commit_tag_gitcli || commit_tag_github || commit_tag_gitlab || "";
 };
